4386e4f3de40a711080392db1d457c5dca171c2d
nakkavijay <nakkavijaykumar1@gmail.com>
2024-10-28 18:01:22 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/universitydb");
+
+
+//check database connected or not
+
+connect.then(()=> {
+    console.log("Database connected Successfully");
+
+})
+.catch(() => {
+    console.log("Database cannot be connected");
+});
+
+
+// Create a schema
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//collection Part
+
+const collection = new mongoose.model("department", LoginSchema);
+
+module.exports = collection;
+
+
+
+
